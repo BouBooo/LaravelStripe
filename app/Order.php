50fa061165f4193e31e9abe3d4ebb92019c24e9c
BouBooo <florent.nicolas@ynov.com>
2020-01-28 13:51:42 +0100

    order model & relations

---
+++ app/Order.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'user_id', 'paiement_email', 'paiement_country', 'paiement_address', 'paiement_city',
+        'paiement_card_name', 'paiement_discount', 'paiement_subtotal', 'paiement_tax', 'paiement_total',
+        'error'
+    ];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function products() {
+        return $this->belongsToMany('App\Products'->withPivot('quantity');
+    }
+}
---
+++ database/migrations/2020_01_28_110319_create_orders_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('set null');
+            $table->string('paiement_email');
+            $table->string('paiement_country');
+            $table->string('paiement_address');
+            $table->string('paiement_city');
+            $table->string('paiement_card_name');
+            $table->string('paiement_discount')->nullable();
+            $table->string('paiement_subtotal');
+            $table->string('paiement_tax');
+            $table->string('paiement_total');
+            $table->string('error');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
